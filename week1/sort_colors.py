339780435518bee44c2719d6985a5a679ef1a7d3
Yohans Hailu <95067604+YohansHailu@users.noreply.github.com>
2021-12-11 15:08:58 +0300

    Create sort_colors.py

---
+++ week1/sort_colors.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)-1):
+            
+            min_index = i
+            for j in range(i+1,len(nums)):
+                if nums[j]<nums[min_index]:
+                    min_index = j
+            
+            #swap them
+            temp = nums[i]
+            nums[i] = nums[min_index]
+            nums[min_index] = temp
+            
+        
