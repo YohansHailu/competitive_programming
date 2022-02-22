9e7e0f692a60bd6a0d164111ef1450948893883f
Yohans Hailu <95067604+YohansHailu@users.noreply.github.com>
2022-02-22 13:07:33 +0300

    Update Top_K_Frequent_Elementary.py

--- camp/Top_K_Frequent_Elementary.py
+++ camp/Top_K_Frequent_Elementary.py
@@ -1,24 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = dict()
-        for i in nums:
-            count[i] = count.get(i,0) + 1
-
-        tw = 0
-        ans = []
+        freqDict = Counter(nums)
         
-        d = [ [] [] [] ]
-        d.sort(key = lambda x: x[1])
+        ans = [] 
         
-        while tw <k:
-            max_value = None 
-            max_count = -1
-            for i in count:
-                if count[i] > max_count:
-                    max_count = count[i]
-                    max_value = i
-            ans.append(max_value)
-            tw+=1
-            count[ max_value ] = -1
-
-        return (ans)
+        for val in freqDict:
+            if len(ans) < k:
+                heappush(ans, (freqDict[val], val) )
+                continue
+            
+            heappushpop(ans,(freqDict[val], val))
+            
+        return [i[1] for i in ans]
+        
+        
+        
