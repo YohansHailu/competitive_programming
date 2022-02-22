3055e8c3d577bd1ad044ae3871ea96f57827027c
Yohans Hailu <95067604+YohansHailu@users.noreply.github.com>
2022-02-22 13:02:20 +0300

    Create top_k_frequent.py

---
+++ camp/top_k_frequent.py
@@ -0,0 +1,14 @@
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freqDict = Counter(nums)
+        
+        ans = [] 
+        
+        for val in freqDict:
+            if len(ans) < k:
+                heappush(ans, (freqDict[val], val) )
+                continue
+            
+            heappushpop(ans,(freqDict[val], val))
+            
+        return [i[1] for i in ans]
+        
