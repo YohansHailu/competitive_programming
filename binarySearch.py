e459f3d67bdccd0334c5ae515c6ec453ecb53a35
Yohans Hailu <95067604+YohansHailu@users.noreply.github.com>
2022-02-24 15:58:13 +0300

    Create binarySearch.py

---
+++ binarySearch.py
@@ -0,0 +1,13 @@
+  def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+
+      cur = root
+
+      while(cur != None):
+          if cur.val == val:
+              return cur
+          elif val > cur.val:
+              cur = cur.right
+          elif val < cur.val:
+              cur = cur.left
+
+      return None 
